914ccc27034d7cc63a0ad172a96782b8f6d0e7de
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-17 15:13:23 +0300

    Add new urls

--- social_media_service/urls.py
+++ social_media_service/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls", namespace="users")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-
-from users.views import CreateUserView
+from users.views import CreateUserView, ManageUserView, LogoutUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("logout/", LogoutUserView.as_view(), name="logout"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+    path("me/", ManageUserView.as_view(), name="manage-user"),
 ]
