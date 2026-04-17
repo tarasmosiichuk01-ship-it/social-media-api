d28062db10235d226edb1d8951592b7f364cd3b4
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-17 15:10:59 +0300

    Create UserDetailSerializer, UserListSerializer, LogoutSerializer, AuthTokenSerializer and improve UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,92 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import gettext as _
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "style": {"input_type": "password"},
+                "label": _("Password"),
+            }
+        }
 
     def create(self, validated_data):
         """create user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """update user with encrypted password"""
+        password = validated_data.pop("password")
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email"), write_only=True)
+    password = serializers.CharField(
+        label=_("Password"),
+        style={"input_type": "password"},
+        trim_whitespace=False,
+        write_only=True,
+    )
+    token = serializers.CharField(label=_("Token"), read_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get("request"), email=email, password=password
+            )
+
+            if not user:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _("Must include 'email' and 'password'.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        try:
+            self.token = RefreshToken(attrs["refresh"])
+        except TokenError:
+            raise serializers.ValidationError("Invalid or expired token")
+        return attrs
+
+    def save(self, **kwargs):
+        self.token.blacklist()
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "avatar")
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "first_name", "last_name", "bio", "avatar")
