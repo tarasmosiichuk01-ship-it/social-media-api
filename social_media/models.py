ea32c17c7c3032380bcc03f0c558605140629a67
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-21 10:47:30 +0300

    Create Post, Hashtag, Like, Comment models in social_media\models.py

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,36 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    content = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    hashtags = models.ManyToManyField("Hashtag", blank=True, related_name="posts")
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta:
+        unique_together = ("user", "post")
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
